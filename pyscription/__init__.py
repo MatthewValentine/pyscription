023049aed22127381ee506e0773f450660f58133
Matthew Valentine <matthew.aaron.valentine+github@gmail.com>
2019-09-07 13:31:53 -0700

    Export command_alias

--- pyscription/__init__.py
+++ pyscription/__init__.py
@@ -3,4 +3,4 @@ name = 'pyscription'
 __version__ = '0.0.1'
 
 from . import awk, command, config, fmt, log, shell, util
-from .command import alias, Command, command, run
+from .command import alias, Command, command, command_alias, run
